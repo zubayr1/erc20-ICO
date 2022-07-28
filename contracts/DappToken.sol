7956328fb729c2cc7ec723e843cc1c195d38c28c
zubayr1 <write2zubayr@gmail.com>
2022-07-28 12:12:32 +0200

    add dapp token sol and test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract DappToken
+{
+    uint256 public totalSupply;
+
+    //constructor
+    constructor(uint256 totalsupply)
+  {
+    totalSupply = totalsupply;
+  }
+
+
+    // set total number of tokens
+
+    //read the total number of tokens
+}
